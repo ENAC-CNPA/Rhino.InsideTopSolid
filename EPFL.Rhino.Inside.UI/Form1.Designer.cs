819bd5adb0670376de95f07a54de8ec9826f5927
regeew <56161668+regeew@users.noreply.github.com>
2021-02-22 10:56:30 +0100

    Trying to launch a RhinoWindow...

---
+++ EPFL.Rhino.Inside.UI/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace EPFL.RhinoInsideTopSolid.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewportControl1 = new RhinoWindows.Forms.Controls.ViewportControl();
+            this.SuspendLayout();
+            // 
+            // viewportControl1
+            // 
+            this.viewportControl1.Location = new System.Drawing.Point(1, 1);
+            this.viewportControl1.Name = "viewportControl1";
+            this.viewportControl1.Size = new System.Drawing.Size(798, 447);
+            this.viewportControl1.TabIndex = 0;
+            this.viewportControl1.Text = "viewportControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.viewportControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RhinoWindows.Forms.Controls.ViewportControl viewportControl1;
+    }
+}
