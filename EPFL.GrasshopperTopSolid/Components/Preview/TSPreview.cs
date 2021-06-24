257fbc12e3dc1c6f0eda876f53b2aa2ff8e97efc
Ahmed Wael Ismail <77401227+ahmedwael94@users.noreply.github.com>
2021-06-24 11:48:31 +0200

    Preview GH surface in TopSolid supported

--- EPFL.GrasshopperTopSolid/Components/Preview/TSPreview.cs
+++ EPFL.GrasshopperTopSolid/Components/Preview/TSPreview.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-
-using Grasshopper.Kernel;
-using Rhino.Geometry;
-
-using TopSolid.Kernel.GR.Displays;
-using TopSolid.Kernel.DB.D3.Documents;
-using TopSolid.Kernel.GR.D3;
-using TopSolid.Kernel.GR.Attributes;
-using TopSolid.Kernel.SX.Drawing;
+﻿using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TopSolid.Kernel.DB.D3.Documents;
+using TopSolid.Kernel.GR.Attributes;
+using TopSolid.Kernel.GR.D3;
+using TopSolid.Kernel.GR.Displays;
+using TopSolid.Kernel.SX.Drawing;
 
 namespace EPFL.GrasshopperTopSolid.Components
 {
@@ -30,25 +29,25 @@ namespace EPFL.GrasshopperTopSolid.Components
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddGeometryParameter("Geometries", "G", "Geometries to display", GH_ParamAccess.list);            
+            pManager.AddGeometryParameter("Geometries", "G", "Geometries to display", GH_ParamAccess.list);
         }
 
         /// <summary>
         /// Registers all the output parameters for this component.
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
-        {            
+        {
         }
-               
+
         private GeometricDocument doc = TopSolid.Kernel.UI.Application.CurrentDocument as GeometricDocument;
         private GeneralDisplay gd = new GeneralDisplay(null);
-        
+
         protected override void BeforeSolveInstance()
         {
             gd.Clear();
             base.BeforeSolveInstance();
         }
-                
+
         /// <summary>
         /// This is the method that actually does the work.
         /// </summary>
@@ -56,29 +55,29 @@ namespace EPFL.GrasshopperTopSolid.Components
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             List<IGH_GeometricGoo> geo = new List<IGH_GeometricGoo>();
-            
-            if (!DA.GetDataList(0, geo)) { return; }            
+
+            if (!DA.GetDataList(0, geo)) { return; }
 
             if (geo == null) { return; }
             if (geo.Count == 0) { return; }
 
 
-        if (!doc.Display.ContainsDisplay(gd))
-        {
-            doc.Display.AddDisplay(gd);
-        }
+            if (!doc.Display.ContainsDisplay(gd))
+            {
+                doc.Display.AddDisplay(gd);
+            }
 
 
-        foreach (var g in geo)
+            foreach (var g in geo)
             {
                 if (g is GH_Point gp)
                 {
-                    var rp = new Point3d();                    
+                    var rp = new Point3d();
                     GH_Convert.ToPoint3d(gp, ref rp, 0);
                     var tp = rp.ToHost();
                     MarkerItem mi = new MarkerItem(tp);
                     mi.Color = Color.Green;
-                    mi.MarkerStyle = MarkerStyle.ExtraLargePlus;                    
+                    mi.MarkerStyle = MarkerStyle.ExtraLargePlus;
                     gd.Add(mi);
                 }
                 else if (g is GH_Line gl)
@@ -97,9 +96,26 @@ namespace EPFL.GrasshopperTopSolid.Components
                 //    GH_Convert.ToCurve(gc, ref rc, 0);
                 //    rp = rc.ToPolyline(0.01, 0.01, 0.01, 0.05);
                 //    var tc = rc.ToHost();
-                    
-                //}                    
-            }                              
+
+                //}
+
+                else if (g is GH_Surface srf)
+                {
+                    Rhino.Geometry.Surface _srf = null;
+                    GH_Convert.ToSurface(srf, ref _srf, GH_Conversion.Both);
+                    var x = Convert.ToHost(_srf.ToNurbsSurface());
+                    TopSolid.Kernel.DB.D3.Surfaces.SurfaceEntity srfentity = new TopSolid.Kernel.DB.D3.Surfaces.SurfaceEntity(doc, 0);
+                    srfentity.Geometry = x;
+                    if (srfentity.Display.Items.Count != 0)
+
+                    {
+                        for (int i = 0; i < srfentity.Display.Items.Count; i++)
+                        { gd.Add(srfentity.Display.Items.ElementAt(i)); }
+                    }
+
+
+                }
+            }
         }
 
         /// <summary>
