b066dc22f8720644b64a6e8de53341e357e04e51
Ahmed Wael Ismail <77401227+AhWaIs@users.noreply.github.com>
2023-10-03 10:03:34 +0200

    Update Version.cs

--- EPFL.Rhino.Inside.UI/Version.cs
+++ EPFL.Rhino.Inside.UI/Version.cs
@@ -23,7 +23,7 @@ namespace EPFL.RhinoInsideTopSolid.UI
 
         /// </summary>
 
-        public static readonly TK.SX.Version Current = new TK.SX.Version(7, 17, 400, 4); // 2023-05-26: …
+        public static readonly TK.SX.Version Current = new TK.SX.Version(7, 17, 400, 5); // 2023-10-03: …
 
 
 
@@ -33,7 +33,7 @@ namespace EPFL.RhinoInsideTopSolid.UI
 
         /// </summary>
 
-        public const string CurrentAssemblyVersionString = "7.17.400.004";
+        public const string CurrentAssemblyVersionString = "7.17.400.005";
 
     }
 }
